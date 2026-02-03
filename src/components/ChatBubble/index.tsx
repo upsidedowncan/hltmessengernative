1f4040909058531a2bb83f964e1adf6e41556e08
HLT Developer <developer@hlt.app>
2026-02-03 11:02:06 +0300

    refactor: remove old src/ directory after migration

--- src/components/ChatBubble/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default } from './index.android';
-export * from './ChatBubble.types';
--- src/components/ui/HostWrapper.tsx
+++
@@ -1,5 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-
-export function HostWrapper({ children }: PropsWithChildren) {
-  return <>{children}</>;
-}
